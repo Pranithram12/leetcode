9670701a9e9eeb3fc0fc819287a8062ffcd95295
Pranithram12 <bingipranithram@gmail.com>
2024-03-18 19:34:37 +0530

    added java file and explanation text file

---
+++ 525. Contiguous Array/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+      Map<Integer,Integer> map = new HashMap<>();
+      map.put(0,-1);
+      int sum=0;
+      int maxlength=0;
+      for(int i=0;i < nums.length;i++){
+          if(nums[i]==0){
+              nums[i]=-1;
+          }   
+      }
+      for(int i=0;i<nums.length;i++){
+         sum +=nums[i];
+         if(map.containsKey(sum)){
+          maxlength=Math.max(maxlength,i-map.get(sum));
+         }
+         else{
+          map.put(sum,i);
+         }
+      }
+      return maxlength;
+    }
+}
