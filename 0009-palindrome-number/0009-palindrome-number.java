1815f1b73b62d9d7aea3770ec129d514c0d3e231
Pranithram12 <161814904+Pranithram12@users.noreply.github.com>
2024-06-05 09:34:06 +0530

    Time: 6 ms (37.65%), Space: 44.1 MB (23.16%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        // Convert the integer to a string
+        String strX = String.valueOf(x);
+
+        // Use two pointers approach to check for palindrome
+        int left = 0;
+        int right = strX.length() - 1;
+
+        while (left < right) {
+            if (strX.charAt(left) != strX.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;  
+    }
+}
